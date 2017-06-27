14269fd5149a6983bcd26af1d2837c5980c116be
Silvio <Silvio@Silvio-PC>
2017-06-27 21:52:26 +0200

    Some classes more

--- GameDevelopment/src/Game.java
+++ GameDevelopment/src/Game.java
@@ -5,60 +5,72 @@ import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
-public class Game extends JFrame{
-	
+/*
+ * BufferStrategy - A mechanism to handle our Graphics Buffering
+ * 				  - Allows us to swap and access our Graphics Buffers
+ * 
+ * Canvas - A JFrame component that we can draw our graphics to.
+ * 		  - Handles our BufferSTrategy , it handles the switching
+ * */
+
+public class Game extends JFrame implements Runnable {
+
 	private Canvas canvas = new Canvas();
-	
-	public Game(){
-		
-		//Makes our program to shut down when we exit out.
+
+	public Game() {
+
+		// Makes our program to shut down when we exit out.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Set the position and size of our frame.
-		setBounds(0,0, 1000,800);
-		//Put our frame in the center of the screen
+
+		// Set the position and size of our frame.
+		setBounds(0, 0, 1000, 800);
+		// Put our frame in the center of the screen
 		setLocationRelativeTo(null);
-		//Add our graphics component
+		// Add our graphics component
 		add(canvas);
-		//Make our frame visible.
-				setVisible(true);
-		//Create our object for buffer strategy
+		// Make our frame visible.
+		setVisible(true);
+		// Create our object for buffer strategy
 		canvas.createBufferStrategy(2);
-		BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+
+	}
+
+	public static void main(String[] args) {
 		
+		Game game = new Game();
+		Thread gameThread = new Thread(game);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		BufferStrategy bufferStrategy = canvas.getBufferStrategy();
 		int i = 0;
 		int x = 5;
-		//Bad loop, dont do it
-		while(true){
+		// Bad loop, dont do it
+		while (true) {
 			i++;
-			if(i == 10){
+			if (i == 10) {
 				i = 0;
 				x++;
 			}
 			bufferStrategy = canvas.getBufferStrategy();
 			Graphics graphics = bufferStrategy.getDrawGraphics();
 			super.paint(graphics);
+
+			// Painting the Background
+			graphics.setColor(Color.black);
+			graphics.fillRect(0, 0, getWidth(), getHeight());
+
+			// Painting the Oval
 			graphics.setColor(Color.red);
 			graphics.fillOval(x, 500, 100, 100);
+
 			graphics.dispose();
 			bufferStrategy.show();
-			
-		}
-	}
-	
-	/*public void paint(Graphics graphics){
-		super.paint(graphics);
-		graphics.setColor(Color.red);
-		graphics.fillOval(300, 500, 100, 100);
-		
-	}*/
-	
-	public static void main(String[] args){
-		
-		new Game();
-	}
-	
-	
-	
 
+		}
+
+	}
 }
