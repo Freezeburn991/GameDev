9d6ea556cf6b107e811ffe93ae2627700872dc90
Silvio <Silvio@Silvio-PC>
2017-06-28 21:12:15 +0200

    Bla Bla jebote konj kad ce proraditi to normalno

--- GameDevelopment/src/Game.java
+++ GameDevelopment/src/Game.java
@@ -1,76 +1,103 @@
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
-
+import java.awt.Graphics;
+import java.lang.Runnable;
+import java.lang.Thread;
 import javax.swing.JFrame;
 
-/*
- * BufferStrategy - A mechanism to handle our Graphics Buffering
- * 				  - Allows us to swap and access our Graphics Buffers
- * 
- * Canvas - A JFrame component that we can draw our graphics to.
- * 		  - Handles our BufferSTrategy , it handles the switching
- * */
-
-public class Game extends JFrame implements Runnable {
-
+public class Game extends JFrame implements Runnable
+{
 	private Canvas canvas = new Canvas();
+	private RenderHandler renderer;
 
-	public Game() {
-
-		// Makes our program to shut down when we exit out.
+	public Game() 
+	{
+		//Make our program shutdown when we exit out.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Set the position and size of our frame.
-		setBounds(0, 0, 1000, 800);
-		// Put our frame in the center of the screen
+		//Set the position and size of our frame.
+		setBounds(0,0, 1000, 800);
+
+		//Put our frame in the center of the screen.
 		setLocationRelativeTo(null);
-		// Add our graphics component
+
+		//Add our graphics compoent
 		add(canvas);
-		// Make our frame visible.
+
+		//Make our frame visible.
 		setVisible(true);
-		// Create our object for buffer strategy
-		canvas.createBufferStrategy(2);
+
+		//Create our object for buffer strategy.
+		canvas.createBufferStrategy(3);
+
+		renderer = new RenderHandler(getWidth(), getHeight());
 
 	}
 
-	public static void main(String[] args) {
-		
+	
+	public void update() {
+
+	}
+
+
+	public void render() {
+			BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+			Graphics graphics = bufferStrategy.getDrawGraphics();
+			super.paint(graphics);
+
+			renderer.render(graphics);
+
+			graphics.dispose();
+			bufferStrategy.show();
+	}
+
+	public void run() {
+		BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+		int i = 0;
+		int x = 0;
+
+		long lastTime = System.nanoTime(); //long 2^63
+		double nanoSecondConversion = 1000000000.0 / 60; //60 frames per second
+		double changeInSeconds = 0;
+
+		while(true) {
+			long now = System.nanoTime();
+
+			changeInSeconds += (now - lastTime) / nanoSecondConversion;
+			while(changeInSeconds >= 1) {
+				update();
+				changeInSeconds = 0;
+			}
+
+			render();
+			lastTime = now;
+		}
+
+		//Bad loop
+		// while(true) {
+		// 	bufferStrategy = canvas.getBufferStrategy();
+		// 	Graphics graphics = bufferStrategy.getDrawGraphics();
+		// 	super.paint(graphics);
+
+		// 	//Painting the Backround
+		// 	graphics.setColor(Color.black);
+		// 	graphics.fillRect(0, 0, getWidth(), getHeight());
+
+		// 	//Painting the Oval
+		// 	graphics.setColor(Color.red);		
+		// 	graphics.fillOval(x, 200, 50, 100);
+
+		// 	graphics.dispose();
+		// 	bufferStrategy.show();
+		// }
+	}
+
+	public static void main(String[] args) 
+	{
 		Game game = new Game();
 		Thread gameThread = new Thread(game);
 		gameThread.start();
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		BufferStrategy bufferStrategy = canvas.getBufferStrategy();
-		int i = 0;
-		int x = 5;
-		// Bad loop, dont do it
-		while (true) {
-			i++;
-			if (i == 10) {
-				i = 0;
-				x++;
-			}
-			bufferStrategy = canvas.getBufferStrategy();
-			Graphics graphics = bufferStrategy.getDrawGraphics();
-			super.paint(graphics);
-
-			// Painting the Background
-			graphics.setColor(Color.black);
-			graphics.fillRect(0, 0, getWidth(), getHeight());
-
-			// Painting the Oval
-			graphics.setColor(Color.red);
-			graphics.fillOval(x, 500, 100, 100);
-
-			graphics.dispose();
-			bufferStrategy.show();
-
-		}
-
-	}
 }
